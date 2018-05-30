5f7db35f5e7aaefbeb7abde3ffca47ea7eea219b
Gilson Tsang <37494996+gilsontsang@users.noreply.github.com>
2018-05-30 07:45:21 -0700

    Update 5. Todo List.js

--- 5. Todo List.js
+++ 5. Todo List.js
@@ -1,25 +1,19 @@
 /* 
-
 Check off specific todos by clicking
 Previous code:
 $("li").click(function() {
 	$(this).toggleClass("completed");
 });
-
 If we swap $("li").click() for $("ul").on("click, li"), then this will handle all future potential li's in ul's. Instead, previously, the code is only for pre-existing li's.
-
 */
 $("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
 
 /* 
-
 Click on X to first fade out then remove the parent 'li'. 
-
 Similarly, previous code:
 $("span").click(...)
-
 */
 $("ul").on("click", "span", function(event) {
 	$(this).parent().fadeOut(500,function() {
@@ -36,6 +30,11 @@ $("input[type='text']").keypress(function(event) {
     // clear the input
     $(this).val("");
   	// create a new li and add to ul through 'appending'
-    $("ul").append("<li><span>X </span>" + toDoText + "</li>");
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span>" + toDoText + "</li>");
   }
+})
+
+// Toggle hiding the input when we press fa-plus
+$(".fa-plus").click(function() {
+	$("input[type=text]").fadeToggle();
 })
